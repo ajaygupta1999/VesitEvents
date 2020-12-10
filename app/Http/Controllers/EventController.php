1dadcd805f50decabe730ca76b11d0d85b6b2026
bighac <2018.ajay.gupta@ves.ac.in>
2020-12-10 19:41:03 +0530

    small change

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -89,26 +89,26 @@ class EventController extends Controller
         return redirect("/");
     }
 
-    // public function smsSend(){
-    //     // $basic  = new \Nexmo\Client\Credentials\Basic('208c20c2', 'dOEtRew2SEbYZwzS');
-    //     // $client = new \Nexmo\Client($basic);
+    // // public function smsSend(){
+    // //     // $basic  = new \Nexmo\Client\Credentials\Basic('208c20c2', 'dOEtRew2SEbYZwzS');
+    // //     // $client = new \Nexmo\Client($basic);
 
-    //     // $message = $client->message()->send([
-    //     //     'to' => '9136276661',
-    //     //     'from' => 'Nexmo',
-    //     //     'text' => 'A text message sent using the Nexmo SMS API'
-    //     // ]);
+    // //     // $message = $client->message()->send([
+    // //     //     'to' => '9136276661',
+    // //     //     'from' => 'Nexmo',
+    // //     //     'text' => 'A text message sent using the Nexmo SMS API'
+    // //     // ]);
 
-        $account_sid = getenv("TWILIO_SID");
-        $auth_token = getenv("TWILIO_AUTH_TOKEN");
-        $twilio_number = getenv("TWILIO_NUMBER");
-        $client = new Client($account_sid, $auth_token);
-        $client->messages->create(
-            "+918097913880", 
-            ['from' => $twilio_number, 'body' => "Simple verification message"] );
+    //     $account_sid = getenv("TWILIO_SID");
+    //     $auth_token = getenv("TWILIO_AUTH_TOKEN");
+    //     $twilio_number = getenv("TWILIO_NUMBER");
+    //     $client = new Client($account_sid, $auth_token);
+    //     $client->messages->create(
+    //         "+918097913880", 
+    //         ['from' => $twilio_number, 'body' => "Simple verification message"] );
         
-        return redirect("/");
-    // }
+    //     return redirect("/");
+    // // }
 
     function removeEvent(){
         DB::rollBack();
