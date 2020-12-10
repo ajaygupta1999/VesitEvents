939531a04eec4a718bfc90138e6accbf575f9472
Neel Bhagat <neelnb11@gmail.com>
2020-12-10 17:56:03 +0530

    updt

--- app/Console/Commands/sendReminders.php
+++ app/Console/Commands/sendReminders.php
@@ -40,27 +40,27 @@ class sendReminders extends Command
      */
     public function handle()
     {
-        $events = Event::where('date',date('Y-m-d'))->get();
-        foreach ($events as $event){
-            $users_registers = Register::where('event_id',$event->id)
-//                ->where('send_remainder',0)
-                ->get();
-            foreach ($users_registers as $users_register){
-                $user = User::find($users_register->user_id);
-//                $users_register->send_remainder = 1;
-//                $users_register->save();
-                if($user){
+//        $events = Event::where('date',date('Y-m-d'))->get();
+//        foreach ($events as $event){
+//            $users_registers = Register::where('event_id',$event->id)
+////                ->where('send_remainder',0)
+//                ->get();
+//            foreach ($users_registers as $users_register){
+//                $user = User::find($users_register->user_id);
+////                $users_register->send_remainder = 1;
+////                $users_register->save();
+//                if($user){
                     $basic  = new \Nexmo\Client\Credentials\Basic('208c20c2', 'dOEtRew2SEbYZwzS');
                     $client = new \Nexmo\Client($basic);
                     $message = $client->message()->send([
-                        'to' => $user->phone_number,
+                        'to' => '918291597204',
                         'from' => 'Vesit Events',
-                        'text' => $event->name.' is scheduled on '. $event->date. ' at '. $event->time
+                        'text' => 'jj'
                     ]);
-                }
-
-            }
-        }
-        $this->info('Successfully sent remainder');
+//                }
+//
+//            }
+//        }
+//        $this->info('Successfully sent remainder');
     }
 }
