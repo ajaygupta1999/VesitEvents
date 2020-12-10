4c047853546fa75bfeda83a977e684db9133ee21
Neel Bhagat <neelnb11@gmail.com>
2020-12-10 22:57:17 +0530

    upt

--- app/Console/Commands/sendReminders.php
+++ app/Console/Commands/sendReminders.php
@@ -40,30 +40,36 @@ class sendReminders extends Command
      */
     public function handle()
     {
-        $events = Event::where('date',date('Y-m-d'))->get();
-        foreach ($events as $event){
-            $users_registers = Register::where('event_id',$event->id)
-                ->where('send_remainder',0)
-                ->get();
-            foreach ($users_registers as $users_register){
-                $user = User::find($users_register->user_id);
-              $users_register->send_remainder = 1;
-                $basic  = new \Nexmo\Client\Credentials\Basic('208c20c2', 'dOEtRew2SEbYZwzS');
-                $client = new \Nexmo\Client($basic);
-                try{
-                    $message = $client->message()->send([
-                        'to' => $user->phone_number,
-                        'from' => 'Vesit Events',
-                        'text' => $event->name.' is scheduled on '. $event->date. ' at '. $event->time
-                    ]);
-                    $users_register->save();
-                }
-                catch (\Exception $e){
-                    continue;
-                }
-
-            }
-        }
+//        $events = Event::where('date',date('Y-m-d'))->get();
+//        foreach ($events as $event){
+//            $users_registers = Register::where('event_id',$event->id)
+//                ->where('send_remainder',0)
+//                ->get();
+//            foreach ($users_registers as $users_register){
+//                $user = User::find($users_register->user_id);
+//                $users_register->send_remainder = 1;
+//                $basic  = new \Nexmo\Client\Credentials\Basic('208c20c2', 'dOEtRew2SEbYZwzS');
+//                $client = new \Nexmo\Client($basic);
+//                try{
+//                    $message = $client->message()->send([
+//                        'to' => $user->phone_number,
+//                        'from' => 'Vesit Events',
+//                        'text' => $event->name.' is scheduled on '. $event->date. ' at '. $event->time
+//                    ]);
+//                    $users_register->save();
+//                }
+//                catch (\Exception $e){
+//                    continue;
+//                }
+//            }
+//        }
+        $basic  = new \Nexmo\Client\Credentials\Basic('208c20c2', 'dOEtRew2SEbYZwzS');
+        $client = new \Nexmo\Client($basic);
+        $message = $client->message()->send([
+            'to' => '918291597204',
+            'from' => 'Vesit Events',
+            'text' =>'hi'
+        ]);
         $this->info('Successfully sent remainder');
     }
 }
